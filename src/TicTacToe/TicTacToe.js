ca3a464851ef84b33502b3376e53211166ee3f6d
Joao Nascimento <jasgdn@gmail.com>
2023-01-04 19:50:59 -0300

    game core logic

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -8,7 +8,50 @@ const TicTacToe = () => {
 
   const [cells, setCells] = useState(Array(9).fill(''));
 
+  const [winner, setWinner] = useState();
+
+  const checkForWinner = (squares) => {
+
+    let combos = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+      ],
+      diagnol: [
+        [0, 4, 8],
+        [6, 4, 2]
+      ]
+    }
+
+    for (let combo in combos) {
+      combos[combo].forEach( (pattern) => {
+        if(
+          squares[pattern[0]] === '' ||
+          squares[pattern[1]] === '' ||
+          squares[pattern[2]] === '' 
+          ){
+        //  do nothing
+          }else if(
+      squares[pattern[0]] === squares[pattern[1]] &&
+      squares[pattern[1]] === squares[pattern[2]]
+      ){
+        setWinner(squares[pattern[0]])
+      }
+    })}
+  }
+
   const handleClick = (num) => {
+
+    if(cells[num] !== ''){
+      alert('already clicked')
+      return;
+    }
 
     let squares = [...cells];
 
@@ -22,8 +65,8 @@ const TicTacToe = () => {
       setTurn('x');
     }
 
-    setCells(squares)
-    console.log(squares)
+    checkForWinner(squares);
+    setCells(squares);
   }
 
 
@@ -53,6 +96,12 @@ const TicTacToe = () => {
           </tr>
         </tbody>
       </table>
+      {winner && (
+        <>
+          <p>{winner} is the winner!</p>
+          <button>Play Again</button>
+          </>
+      )}
     </div>
   )
 }
