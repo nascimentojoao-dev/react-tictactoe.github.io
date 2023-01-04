94921337ada97b785d84863abeec41ee479ee5b0
Joao Nascimento <jasgdn@gmail.com>
2023-01-04 19:23:14 -0300

    starting game logic

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -1,30 +1,55 @@
 import React from 'react'
-import './TicTacToe.css'
+import './TicTacToe.css';
+import {useState} from 'react'
 
 const TicTacToe = () => {
-  
-  const Cell = () => {
-    return <td>-</td>
+
+  const [turn, setTurn] = useState('x')
+
+  const [cells, setCells] = useState(Array(9).fill(''));
+
+  const handleClick = (num) => {
+
+    let squares = [...cells];
+
+    if (turn == 'x') {
+
+      squares[num] = 'x'
+      setTurn('o');
+    } else {
+
+      squares[num] = 'o'
+      setTurn('x');
+    }
+
+    setCells(squares)
+    console.log(squares)
   }
-  
+
+
+  const Cell = ({num}) => {
+    return <td onClick={() => handleClick(num)}>{cells[num]}</td>
+  }
+
   return (
     <div className='container'>
       <table>
+        Turn: {turn}
         <tbody>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={0}/>
+            <Cell num={1}/>
+            <Cell num={2}/>
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={3}/>
+            <Cell num={4}/>
+            <Cell num={5}/>
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={6}/>
+            <Cell num={7}/>
+            <Cell num={8}/>
           </tr>
         </tbody>
       </table>
